c2654aa4327a597affb9f07b3833464a2b76b12d
TonyNov <novikov.asn15@gmail.com>
2024-12-02 20:48:45 +0300

    Работа с categories завершена

--- demo/src/main/java/controller/categories/CategoriesController.java
+++ demo/src/main/java/controller/categories/CategoriesController.java
@@ -1,29 +1,42 @@
 package controller.categories;
+
 import java.util.ArrayList;
 import java.util.List;
-public class CategoriesController {
-    public List<CategoryBO> list_cat = new ArrayList<>();
 
-    public void add_cat(CategoryBO new_cat){
-        list_cat.add(new_cat);
+public class CategoriesController {
+    public List<CategoryBO> categoriesList = new ArrayList<>();
+
+    public void addCategory(CategoryBO new_cat) {
+        categoriesList.add(new_cat);
     }
 
-    public List<CategoryBO> get_list_cat(){
+    public List<CategoryBO> getNotEmptyCategories() {
         List<CategoryBO> temp = new ArrayList<>();
-
-        for (int i = 0; i < list_cat.size(); i++) {
-            if (list_cat.get(i).list_n.size() > 0)
-                temp.add(list_cat.get(i));
-        }
+        for (int i = 0; i < categoriesList.size(); i++)
+            if (categoriesList.get(i).size() > 0)
+                temp.add(categoriesList.get(i));
         return temp;
     }
 
-    public void del_cat(int id){
-        list_cat.remove(id);
+    public List<CategoryBO> getAllCategories() {
+        return categoriesList;
     }
 
-    public List<CategoryBO> get_po_slovam(String str){
+    public void getCategory(int id) {
+        categoriesList.remove(id);
+    }
 
+    public void updateCategory(int id, CategoryBO newCategory) {
+        categoriesList.remove(id);
+    }
+
+    public void removeCategory(int id) {
+        categoriesList.remove(id);
+    }
+
+    public List<CategoryBO> getCategoriesByKey(String str) {
+        List<CategoryBO> temp = new ArrayList<>();
+        return temp;
     }
 
 }
--- demo/src/main/java/controller/categories/CategoryBO.java
+++ demo/src/main/java/controller/categories/CategoryBO.java
@@ -1,16 +1,15 @@
 package controller.categories;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import view.api.News;
+import controller.news.NewsItemBO;
 
 public class CategoryBO {
 
     private int id;
     private String name;
-    private final List<News> newsList = new ArrayList<>();
+    private final List<NewsItemBO> newsList = new ArrayList<>();
 
     public CategoryBO(String n) {
         name = n;
@@ -32,35 +31,31 @@ public class CategoryBO {
         return id;
     }
 
-    public void addToList(News value) {
+    public void addToList(NewsItemBO value) {
         newsList.add(value);
     }
 
-    public void addToBeginning(News value) {
+    public void addToBeginning(NewsItemBO value) {
         newsList.add(0, value);
     }
 
-    public boolean removeFirstOccurrence(News value) {
-        return newsList.remove(value);
+    public void removeNewsByID(int newsID) {
+        for (int i = 0; i < newsList.size(); i++)
+            if (newsList.get(i).getId() == newsID) {
+                newsList.remove(i);
+                return;
+            }
     }
 
-    public void removeAllOccurrences(News value) {
-        newsList.removeAll(Collections.singleton(value));
-    }
-
-    public void replaceAllWith(News newValue) {
-        newsList.replaceAll(v -> newValue);
-    }
-
-    public int indexOf(News value) {
+    public int indexOf(NewsItemBO value) {
         return newsList.indexOf(value);
     }
 
-    public int lastIndexOf(News value) {
+    public int lastIndexOf(NewsItemBO value) {
         return newsList.lastIndexOf(value);
     }
 
-    public boolean contains(News value) {
+    public boolean contains(NewsItemBO value) {
         return newsList.contains(value);
     }
 
