5bc7b7c01fa77d100f2744cc819284fe733428f4
TonyNov <novikov.asn15@gmail.com>
2024-12-02 15:39:38 +0300

    Добавлены категории и их контроллер

--- demo/src/main/java/controller/categories/CategoriesController.java
+++ demo/src/main/java/controller/categories/CategoriesController.java
@@ -1,5 +1,29 @@
 package controller.categories;
-
+import java.util.ArrayList;
+import java.util.List;
 public class CategoriesController {
-    
+    public List<CategoryBO> list_cat = new ArrayList<>();
+
+    public void add_cat(CategoryBO new_cat){
+        list_cat.add(new_cat);
+    }
+
+    public List<CategoryBO> get_list_cat(){
+        List<CategoryBO> temp = new ArrayList<>();
+
+        for (int i = 0; i < list_cat.size(); i++) {
+            if (list_cat.get(i).list_n.size() > 0)
+                temp.add(list_cat.get(i));
+        }
+        return temp;
+    }
+
+    public void del_cat(int id){
+        list_cat.remove(id);
+    }
+
+    public List<CategoryBO> get_po_slovam(String str){
+
+    }
+
 }
--- demo/src/main/java/controller/categories/CategoryBO.java
+++ demo/src/main/java/controller/categories/CategoryBO.java
@@ -1,5 +1,75 @@
 package controller.categories;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import view.api.News;
+
 public class CategoryBO {
-    
+
+    private int id;
+    private String name;
+    private final List<News> newsList = new ArrayList<>();
+
+    public CategoryBO(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void addToList(News value) {
+        newsList.add(value);
+    }
+
+    public void addToBeginning(News value) {
+        newsList.add(0, value);
+    }
+
+    public boolean removeFirstOccurrence(News value) {
+        return newsList.remove(value);
+    }
+
+    public void removeAllOccurrences(News value) {
+        newsList.removeAll(Collections.singleton(value));
+    }
+
+    public void replaceAllWith(News newValue) {
+        newsList.replaceAll(v -> newValue);
+    }
+
+    public int indexOf(News value) {
+        return newsList.indexOf(value);
+    }
+
+    public int lastIndexOf(News value) {
+        return newsList.lastIndexOf(value);
+    }
+
+    public boolean contains(News value) {
+        return newsList.contains(value);
+    }
+
+    public int size() {
+        return newsList.size();
+    }
+
+    public void clear() {
+        newsList.clear();
+    }
+
 }
